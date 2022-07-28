1582d41ec51ef6435b5b5bed4b000549424e4876
tegar <eategar@gmail.com>
2022-07-28 20:59:48 +0700

    finish setup theme

---
+++ lib/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaulMargin = 16.0;
+
+const Color primaryColor = Color(0xff0063F5);
+const Color primaryTextColor = Color(0xff212529);
+const Color secondaryTextColor = Color(0xff343A40);
+const Color subtitleTextColor = Color(0xff6C757D);
+const Color backgroundColorColor = Color(0xffF8F9FA);
+
+
+
+TextStyle headerTextStyle = GoogleFonts.poppins(color: primaryTextColor);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: secondaryTextColor);
+TextStyle subtitleTextStyle = GoogleFonts.poppins(color: subtitleTextColor);
+
+
